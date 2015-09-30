fe9b01d5ff67a037dadb049a7e46267a5c358ac2
Jaume Moral <jaume.moral@gmail.com>
2015-09-30 19:58:06 +0200

    Coses de XSS

--- src/main/java/edu/upc/escert/curs/ComentarServlet.java
+++ src/main/java/edu/upc/escert/curs/ComentarServlet.java
@@ -8,14 +8,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
+
 import edu.upc.escert.curs.repositori.RepositoriFactory;
 import edu.upc.escert.curs.repositori.IRepositoriComentaris;
 
+
 @WebServlet("/comentar")
+@SuppressWarnings("serial")
 public class ComentarServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+
 	private IRepositoriComentaris repositoriComentaris=RepositoriFactory.getRepositoriComentaris();
-	
+	public static PolicyFactory policyHTML = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if (request.getSession().getAttribute("username")==null) {
 			response.sendRedirect("login");
@@ -26,8 +32,8 @@ public class ComentarServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String comentari=request.getParameter("comentari");
+
+		String comentari=policyHTML.sanitize(request.getParameter("comentari"));
 		String autor=request.getParameter("autor");
 		String titol=request.getParameter("titol");
 		repositoriComentaris.afegirComentari(new Comentari(autor,titol,comentari));
--- src/main/webapp/comentaris.jsp
+++ src/main/webapp/comentaris.jsp
@@ -8,15 +8,18 @@
 </head>
 <body>
 	<div class="container">
-		<h1>Comentaris ${param.autor}</h1>
+		<h1>Comentaris <c:out value="${param.autor}"/></h1>
 		<c:forEach items="${comentaris}" var="c">
 			<div class="well">
 				<c:if test="${username==c.autor}" >
 				<a class="btn btn-default pull-right" href="esborrar?id=${c.id}">Esborrar comentari</a>
 				</c:if>
-				<h3>${c.titol}</h3>
+				<h3><c:out value="${c.titol}"/></h3>
 				<p>${c.comentari}</p>
-				<i>Publicat per <a href="?autor=${c.autor}">${c.autor}</a> el dia ${c.data}</i>
+				<c:url value="/comentaris" var="url_autor">
+				<c:param name="autor" value="${c.autor}"/>
+				</c:url>
+				<i>Publicat per <a href="${url_autor}"><c:out value="${c.autor}"></c:out></a> el dia <c:out value="${c.data}"/></i>
 			</div>
 		</c:forEach>
 
